8e1f6afc1193785204631b11d8b43ccc97c8e3f4
Eva Müller <fer0x@gmx.de>
2015-05-19 09:09:05 +0200

    Stand: Praktikum 04

---
+++ JKatzenverein/src/java/classes/formData.java
@@ -0,0 +1,100 @@
+package classes;
+
+import java.util.Date;
+
+public class formData {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Date date;
+    private String category;
+    private String headline;
+    private String message;
+    
+    public formData() { }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public void setHeadline(String headline) {
+        this.headline = headline;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder b = new StringBuilder();
+        
+        b.append("First Name: ");
+        b.append(getFirstName());
+        b.append(System.lineSeparator());
+        b.append("Last Name: ");
+        b.append(getLastName());
+        b.append(System.lineSeparator());
+        b.append("Email: ");
+        b.append(getEmail());
+        b.append(System.lineSeparator());
+        b.append("Date: ");
+        b.append(getDate());
+        b.append(System.lineSeparator());
+        b.append("Category: ");
+        b.append(getCategory());
+        b.append(System.lineSeparator());
+        b.append("Headline: ");
+        b.append(getHeadline());
+        b.append(System.lineSeparator());
+        b.append("Message: ");
+        b.append(getMessage());
+        
+        return b.toString();
+    }
+}
---
+++ JKatzenverein/src/java/servlets/formular.java
@@ -0,0 +1,130 @@
+package servlets;
+
+import classes.formData;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import java.time.Clock;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "formular", urlPatterns = {"/formular"})
+public class formular extends HttpServlet {
+
+    public static List<formData> dataList;
+
+    public formular() {
+        this.dataList = new ArrayList<>();
+    }
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet formular</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet formular at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+        /*
+        System.out.print("First Name: ");
+        System.out.println(request.getParameter("firstName"));
+        
+        System.out.print("Last Name: ");
+        System.out.println(request.getParameter("lastName"));
+        
+        System.out.print("eMail: ");
+        System.out.println(request.getParameter("email"));
+        
+        System.out.print("Date: ");
+        System.out.println(request.getParameter("date"));
+        
+        System.out.print("Category: ");
+        System.out.println(request.getParameter("category"));
+        
+        System.out.print("Headline: ");
+        System.out.println(request.getParameter("headline"));
+        
+        System.out.print("Message: ");
+        System.out.println(request.getParameter("message"));
+        */
+        
+        formData newData = new formData();
+        newData.setFirstName(request.getParameter("firstName"));
+        newData.setLastName(request.getParameter("lastName"));
+        newData.setEmail(request.getParameter("email"));
+        newData.setDate(Date.valueOf(request.getParameter("date")));
+        newData.setCategory(request.getParameter("category"));
+        newData.setHeadline(request.getParameter("headline"));
+        newData.setMessage(request.getParameter("message"));
+        dataList.add(newData);
+        
+        System.out.println("Requests received so far:");
+        for (formData data : dataList) {
+            System.out.println(data);
+            System.out.println("-------------------------------");
+        }
+        
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
